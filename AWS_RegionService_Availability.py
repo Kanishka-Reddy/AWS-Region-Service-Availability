ede3e288811e71774975cb624700fdbbcc223c38
Kanishka-Reddy <kanishkareddy5@yahoo.com>
2022-09-23 21:57:58 +0530

    Add files via upload

--- AWS_RegionService_Availability.py
+++ AWS_RegionService_Availability.py
@@ -1,3 +1,4 @@
+import collections
 import requests
 import json
 from pandas import json_normalize
@@ -8,8 +9,8 @@ url = 'https://api.regional-table.region-services.aws.a2z.com/index.json'
 def handler(event, context):
 
     """
-    First we create a lookup table of services per region using an external API. Then we parse the input json to see if all the services are available 
-    in each region, and return a response that consists of the list of all regions and a boolean.
+    First we create a lookup table of services per region using an external API. Then we parse the input json to see which services are available 
+    in each given region, and return a response that consists of the 2 lists available and not_available that has .
 
     """
 
@@ -37,37 +38,40 @@ def handler(event, context):
      
     
     #start parsing input json for list of services to find
-    services_to_find = json.loads(event['body'])
+    services_to_find = json.loads(event['services_query'])
+    regions_to_query = json.loads(event['regions_query'])
 
     #create new dictionary are_services_available that contains regions as keys and a boolean as values that shows whether all the services are available in a particular region
-    are_services_available = {}
-    for key, val in services_for_each_region.items():
-        available = True
+    availability = {}
+
+    for region in regions_to_query:
+        
+        availability[region] = {'available': [], 'not_available': []}
+        services_available = services_for_each_region[region]
+
         for service in services_to_find:
-            if service not in val:
-                available = False
-        are_services_available[key] = available
+            if service not in services_available:
+                availability[region]['not_available'].append(service)
+            else:
+                availability[region]['available'].append(service)
 
     response = {
         'statusCode': 200,
-        'body': json.dumps({ 'result': are_services_available })
+        'body': json.dumps({'service_availabilty': availability})
     }
 
     return response
 
-#demo_input = { 
-#    "statusCode": 200,
-#    "body" : '["AWS CloudFormation", "AWS Batch"]'
-#}       
-#c = []
-#demo_input = json.dumps(demo_input)
-#p = handler(json.loads(demo_input), c)
-#print(p)
+demo_input = { 
+    "statusCode": 200,
+    "services_query" : '["AWS CloudFormation", "AWS Batch"]',
+    "regions_query" : '["ap-east-1"]'
+}       
+c = []
+demo_input = json.dumps(demo_input)
+p = handler(json.loads(demo_input), c)
+print(p)
             
-#Output format:
-#{'statusCode': 200, 'body': '{"result": {"ap-east-1": true, "ap-northeast-1": true, "ap-northeast-2": true, "ap-south-1": true, 
-# "ap-southeast-2": true, "ca-central-1": true, "eu-west-2": true, "eu-west-3": true, "us-east-1": true, "us-east-2": true, 
-# "ap-southeast-1": true, "eu-central-1": true, "eu-north-1": true, "eu-west-1": true, "us-gov-west-1": true, "us-west-1": true, 
-# "us-west-2": true, "af-south-1": true, "ap-northeast-3": true, "cn-north-1": true, "sa-east-1": true, "ap-southeast-3": true, 
-# "eu-south-1": true, "me-central-1": false, "me-south-1": true, "cn-northwest-1": true, "us-gov-east-1": true}}'}
 
+#Output format:
+#{'statusCode': 200, 'body': '{"service_availabilty": {"ap-east-1": {"available": ["AWS CloudFormation", "AWS Batch"], "not_available": []}}}'}
